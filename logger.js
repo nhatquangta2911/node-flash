49e3869df84ddcff3f4e785ea754afb9d9a6d63e
NhatQuangTa <nhatquangta2911@gmail.com>
2019-04-01 01:53:52 +0700

    Node built-in modules: path, os, fs, events (EventEmitter class), http

--- logger.js
+++ logger.js
@@ -1,7 +1,12 @@
-const log = (name) => {
-   console.log(name);
+const EventEmitter = require('events');
+
+class Logger extends EventEmitter {
+   // Basic f() and additional methods
+   log(name) {
+      console.log(name);
+      // Raise an event 
+      this.emit('messageLogged', { id: 1, url: 'http://' });
+   } 
 }
 
-console.log(module);
-
-module.exports.log = log; 
+module.exports = Logger;
