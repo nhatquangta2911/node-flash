dd11eeb730be823ec22fac53858f4020f0226255
NhatQuangTa <nhatquangta2911@gmail.com>
2019-04-02 22:43:33 +0700

    Complete Section03-npm

---
+++ node-app/index.js
@@ -0,0 +1,4 @@
+const ryan = require('ryan-lib');
+
+console.log(ryan.add(29, 11));
+console.log(ryan.multiply(29, 11));
---
+++ ryan-lib/index.js
@@ -0,0 +1,2 @@
+module.exports.add = (a, b) => a + b;
+module.exports.multiply = (a, b) => a * b;
