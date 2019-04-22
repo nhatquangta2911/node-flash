4af8571bcf95eaad114abff0c3eb72f83b188639
NhatQuangTa <nhatquangta2911@gmail.com>
2019-04-22 09:58:59 +0700

    this keyword in JS

---
+++ 0-javascript/0-3-this-keyword/index.js
@@ -0,0 +1,34 @@
+
+/*
+
+TODO: Rule1: method -> obj
+      Rule2: function -> global object (window in browser, global in node)
+
+*/
+
+// TODO: Rule 1
+const video = {
+   title: 'NodeJS',
+   tags: ['A', 'B', 'C'],
+   play() {
+      console.log(this.title);
+   },
+   showTags() {
+      this.tags.forEach(function(tag) {
+         console.log(this.title, tag);
+      }, this); 
+   }
+
+}
+
+video.play();
+video.showTags();
+
+
+//TODO: Rule 2
+
+function playVideo() {
+   console.log(this.global.process.versions.v8);
+}
+
+playVideo();
