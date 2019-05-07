d8906152386a89c5cd83c94986830738efe65c25
NhatQuangTa <nhatquangta2911@gmail.com>
2019-05-07 16:15:24 +0700

    Promise.all([]) and Promise.race([]) for parallel promises

--- 6-async-javascript/6-2-promises/promise-api.js
+++ 6-async-javascript/6-2-promises/promise-api.js
@@ -1,3 +1,4 @@
+// settled promises
 // static method called resolve() 
 // which returning a promise that was already resolved or rejected
 const resolvedPromise = Promise.resolve({ id: 1, name: 'Ryan' });
@@ -5,3 +6,35 @@ resolvedPromise.then(user => console.log(user));
 
 const rejectedPromise = Promise.reject(new Error('error message'));
 rejectedPromise.catch(err => console.log(err.message));
+
+// parallel promises
+
+const promiseFb = new Promise((resolve, reject) => {
+   setTimeout(() => {
+      console.log('Get Fb account information...');
+      resolve({ id: 1, name: 'Ryan', age: 22 });
+   }, 1000);
+})
+
+const promiseMelOn = new Promise((resolve, reject) => {
+   setTimeout(() => {
+      console.log('Get MelOn account information...');
+      reject(new Error('Oop!'));
+      // resolve({ id: 230929, name: 'Ryan', isVipAccount: false });
+   }, 2000);
+})
+
+const promiseHanteo = new Promise((resolve, reject) => {
+   setTimeout(() => {
+      console.log('Get Hanteo account information...');
+      resolve({ id: 58992, name: 'Ryan', isVipAccount: true });
+   }, 3000);
+})
+
+Promise.all([promiseFb, promiseMelOn, promiseHanteo])
+   .then(result => console.log(result))
+   .catch(err => console.log(err.message ));
+
+// Promise.race([promiseFb, promiseMelOn, promiseHanteo])
+//    .then(result => console.log(result))
+//    .catch(err => console.log(err.message ));
