dfd4d4bdd10b8f5dd9ac13a4ebed54a2e2f09210
NhatQuangTa <nhatquangta2911@gmail.com>
2019-05-19 11:02:37 +0700

    Updating document Update First + Retrieve + Demo  operator

--- 7-mongoDB-exercise/exercise-3.js
+++ 7-mongoDB-exercise/exercise-3.js
@@ -1,27 +1,31 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-mongoose.connect('mongodb://localhost/mongo-exercises', { useNewUrlParser: true })
-   .then(() => console.log('Connected!'))
-   .catch(err => console.error('Something went wrong! ', err));
+mongoose
+   .connect("mongodb://localhost/mongo-exercises", {
+      useNewUrlParser: true,
+      useFindAndModify: false
+   })
+   .then(() => console.log("Connected!"))
+   .catch(err => console.error("Something went wrong! ", err));
 
 const courseSchema = mongoose.Schema({
    name: String,
    author: String,
    isPublished: Boolean,
    price: Number,
-   tags: [ String ],
+   tags: [String],
    date: { type: Date, default: Date.now }
 });
 
 const exampleCourse = {
-   name: 'Bullet Journal Course by Ryan',
-   author: 'Ryan',
+   name: "Bullet Journal Course by Ryan",
+   author: "Ryan",
    isPublished: true,
    price: 24,
-   tags: [ 'effective', 'study' ],
-}
+   tags: ["effective", "study"]
+};
 
-const Course = mongoose.model('courses', courseSchema);
+const Course = mongoose.model("courses", courseSchema);
 
 async function createCourse() {
    const course = new Course(exampleCourse);
@@ -30,16 +34,12 @@ async function createCourse() {
 }
 
 async function getCourses() {
-   return await Course
-      .find({
-         isPublished: true
-      })
-      .or([
-         { price: {$gte: 15} },
-         { name: /.*by.*/gi }
-      ])
-      .sort('-price')
-      .select('name author price');
+   return await Course.find({
+      isPublished: true
+   })
+      .or([{ price: { $gte: 15 } }, { name: /.*by.*/gi }])
+      .sort("-price")
+      .select("name author price");
 }
 
 async function displayCourses() {
@@ -50,11 +50,11 @@ async function displayCourses() {
 //TODO: Query fisrt
 async function updateCourseByQueryingFirst(id) {
    const course = await Course.findById(id);
-   if(!course) return;
+   if (!course) return;
 
    course.isPublished = false;
-   course.author = 'Anonymous';
-   
+   course.author = "Anonymous";
+
    const result = await course.save();
    console.log(result);
 }
@@ -63,15 +63,26 @@ async function updateCourseByQueryingFirst(id) {
 async function updateCourseByUpdatingFirst(id) {
    const result = await Course.update(
       { _id: id },
-      { $set: { 
-         author: '오마이걸',
-         price: 29
-      } }
-   ); 
+      {
+         $set: {
+            author: "오마이걸",
+            price: 29
+         }
+      }
+   );
    console.log(result);
+}
+
+//TODO: In case you want to update document and retreive it back
+async function updateCourseByUpdatingFirstAndRetrieve(id) {
+   const course = await Course.findByIdAndUpdate(id, {
+      $inc: { price: 1 }
+   });
+   console.log(course);
 }
 
 // createCourse();
 // displayCourses();
 // updateCourseByQueryingFirst('5ce0c9b81e105823bce60d77');
-updateCourseByUpdatingFirst('5ce0c9b81e105823bce60d77');
+// updateCourseByUpdatingFirst('5ce0c9b81e105823bce60d77');
+updateCourseByUpdatingFirstAndRetrieve("5ce0c9b81e105823bce60d77");
