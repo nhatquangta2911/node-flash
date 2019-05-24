0eca16c3d400add7c932adad7c58ab4cbdd73a9f
Nhat Quang Ta <nhatquangta2911@gmail.com>
2019-05-24 13:55:57 +0700

    Practice Middleware functions (morgan for logging HTTP methods status

--- 8-vidly-app/index.js
+++ 8-vidly-app/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const home = require('./routes/home');
 const genres = require('./routes/genres');
+const morgan = require('morgan');
 
 const app = express();
 
 app.use(express.json());
+app.use(morgan('tiny'));
 app.use('/', home);
 app.use('/api/genres', genres);
 
