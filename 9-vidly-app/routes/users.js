4d15287b79a672a0460b788b95d793f0c1e5fa33
NhatQuangTa <nhatquangta2911@gmail.com>
2019-05-26 23:07:18 +0700

    Pagination for getting user info

--- 9-vidly-app/routes/users.js
+++ 9-vidly-app/routes/users.js
@@ -6,6 +6,7 @@ const router = express.Router();
 const {userSchema} = require('../model/user');
 
 const User = mongoose.model('User', userSchema);
+const pageSize = 2;
 
 //TODO: GET
 router.get('/', async (req, res) => {
@@ -17,6 +18,16 @@ router.get('/user/:email', async (req, res) => {
    const user = await User.findOne({email: req.params.email}).select("name email");
    if(!user) return res.status(404).send('Username is not exist');
    res.send(user);
+});
+
+router.get('/page/:page', async (req, res) => {
+   const users = await User
+      .find()
+      .sort('-name')
+      .skip((req.params.page - 1) * pageSize)
+      .select('_id name email')
+      .limit(pageSize);
+   res.send(users);
 });
 
 //TODO: REGISTER
