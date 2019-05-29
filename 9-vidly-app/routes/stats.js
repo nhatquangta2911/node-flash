4db101ba3ac01cbd93ddb14a9e5278883c7c3730
Nhat Quang Ta <nhatquangta2911@gmail.com>
2019-05-29 14:12:30 +0700

    Get random genres

--- 9-vidly-app/routes/stats.js
+++ 9-vidly-app/routes/stats.js
@@ -2,8 +2,10 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 const {rentalSchema} = require('../model/rental');
+const {genreSchema} = require('../model/genre');
 
 const Rental = mongoose.model('Rental', rentalSchema);
+const Genre = mongoose.model('Genre', genreSchema);
 
 //TODO: 5 recent rentals
 router.get('/rentals/recent', async (req, res) => {
@@ -12,6 +14,15 @@ router.get('/rentals/recent', async (req, res) => {
       .sort('-dateOut')
       .limit(5);
    res.send(rentals);
+});
+
+//TODO: Get 3 random genre
+router.get('/genres/randoms', async (req, res) => {
+   const genres = await Genre
+      .aggregate([
+         {$sample: { size: 4 }}
+      ]);
+   res.send(genres);
 });
 
 module.exports = router;
