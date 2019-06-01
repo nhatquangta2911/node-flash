3ffd7754966edb03c3c3fdf52aee389349d627d0
NhatQuangTa <nhatquangta2911@gmail.com>
2019-06-01 15:46:56 +0700

    Add info about err handling

--- 9-vidly-app/index.js
+++ 9-vidly-app/index.js
@@ -22,13 +22,15 @@ const {logger} = require('./middleware/logging');
 //TODO: (Just SYNC) EventEmitter raise an event when node process (no catch) error
 process.on('uncaughtException', (ex) => {
    logger.error(ex.message, ex);
-   process.exit(1);
+   process.exit(1); // exit process as a best practice (cuz of unclean state) 
+   // In production: Tool - process manager - responsible for automatically restart an node process
 });
 
-//TODO: (ASYNC - Promises)
+//TODO: (ASYNC - Promises) 
 process.on('unhandledRejection', (ex) => {
    logger.error(ex.message, ex);
    process.exit(1);
+   // throw an ex => become uncaught exception
 });
 
 logger.info('Starting application...');
