f0a547c7aa52f4bba0623d93fe7180c64f949303
NhatQuangTa <nhatquangta2911@gmail.com>
2019-06-02 21:29:54 +0700

    Test numbers

---
+++ 12-unit-testing/lib.js
@@ -0,0 +1,6 @@
+//TODO: TESTING NUMBERS
+module.exports.absolute = (number) => {
+   if(number > 0) return number;
+   if(number < 0) return -number;
+   return 0;
+}
--- 12-unit-testing/test/lib.test.js
+++ 12-unit-testing/test/lib.test.js
@@ -1,3 +1,18 @@
-test('Very first test', () => {
-   throw new Error('Oh my mistake!');
+const lib = require('../lib');
+
+
+//TODO: A TEST CASE
+test('absolute - should return a positive number if input is positive', () => {
+   const result = lib.absolute(1);
+   expect(result).toBe(1);
+});
+
+test('absolute - should return a positive number if input is negative', () => {
+   const result = lib.absolute(-1);
+   expect(result).toBe(1);
+});
+
+test('absolute - should return a positive number if input is negative', () => {
+   const result = lib.absolute(0);
+   expect(result).toBe(0);
 });
