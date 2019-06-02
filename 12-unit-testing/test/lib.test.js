aa17daa370c739fba65dd3084e42c313bd080fbf
NhatQuangTa <nhatquangta2911@gmail.com>
2019-06-02 21:42:35 +0700

    Grouping testcase into one (using descibe - it)

--- 12-unit-testing/test/lib.test.js
+++ 12-unit-testing/test/lib.test.js
@@ -1,23 +1,26 @@
-const lib = require('../lib');
+const lib = require("../lib");
 
+describe("absolute", () => {
+   //TODO: A TEST CASE
+   it("Should return a positive number if input is positive", () => {
+      const result = lib.absolute(1);
+      expect(result).toBe(1);
+   });
 
-//TODO: A TEST CASE
-test('absolute - should return a positive number if input is positive', () => {
-   const result = lib.absolute(1);
-   expect(result).toBe(1);
+   it("Should return a positive number if input is negative", () => {
+      const result = lib.absolute(-1);
+      expect(result).toBe(1);
+   });
+
+   it("Should return a positive number if input is negative", () => {
+      const result = lib.absolute(0);
+      expect(result).toBe(0);
+   });
 });
 
-test('absolute - should return a positive number if input is negative', () => {
-   const result = lib.absolute(-1);
-   expect(result).toBe(1);
+describe("floating", () => {
+   it("Should return a close number to the actual result", () => {
+      const result = lib.floating(2.9, 1.1);
+      expect(result).toBeCloseTo(4);
+   });
 });
-
-test('absolute - should return a positive number if input is negative', () => {
-   const result = lib.absolute(0);
-   expect(result).toBe(0);
-});
-
-test('floating - Should return a close number to the actual result', () => {
-   const result = lib.floating(2.9, 1.1);
-   expect(result).toBeCloseTo(4);
-})
