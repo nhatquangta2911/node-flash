e35eb4e4a21c39412a09f9c0c51a05b110ca0653
NhatQuangTa <nhatquangta2911@gmail.com>
2019-06-02 21:36:37 +0700

    Test numbers close to float numbers

--- 12-unit-testing/lib.js
+++ 12-unit-testing/lib.js
@@ -4,3 +4,7 @@ module.exports.absolute = (number) => {
    if(number < 0) return -number;
    return 0;
 }
+
+module.exports.floating = (number1, number2) => {
+   return number1 + number2;
+}
--- 12-unit-testing/test/lib.test.js
+++ 12-unit-testing/test/lib.test.js
@@ -16,3 +16,8 @@ test('absolute - should return a positive number if input is negative', () => {
    const result = lib.absolute(0);
    expect(result).toBe(0);
 });
+
+test('floating - Should return a close number to the actual result', () => {
+   const result = lib.floating(2.9, 1.1);
+   expect(result).toBeCloseTo(4);
+})
