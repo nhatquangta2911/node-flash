6641bbebbfe6441bfe3320249c2bcac81703dd30
NhatQuangTa <nhatquangta2911@gmail.com>
2019-06-08 21:33:17 +0700

    Best practice to test r common route - not too general

--- 9-vidly-app/tests/integration/cards.test.js
+++ 9-vidly-app/tests/integration/cards.test.js
@@ -1,18 +1,44 @@
+//TODO: to populate DB, we need import Card model
 const request = require('supertest');
+const mongoose = require('mongoose');
+const {cardSchema} = require('../../model/card');
+
 let server;
+const Card = mongoose.model('Card', cardSchema);
 
 describe('/api/cards', () => {
 
    //TODO: Call this function before each test inside this test suite
    beforeEach(() => { server = require('../../index'); });
-   afterEach(() => { server.close(); });
+   afterEach(async () => { 
+      server.close();
+      await Card.remove({});      
+   });
 
    //TODO: One suite for each route
    describe('GET /', () => {
       it('should return all cards', async () => {
+         await Card.collection.insertMany([
+            {  englishTitle: 'card1',
+               vietnameseTitle: 'thetuvung1',
+               image: 'img1.png',
+               example: 'card1 in your area =))',
+               type: 'IDIOM',
+               context: 'for testing' },
+            {  englishTitle: 'card2',
+               vietnameseTitle: 'thetuvung2',
+               image: 'img2.png',
+               example: 'card2 in your area =))',
+               type: 'IDIOM',
+               context: 'for testing' }
+         ])
          const response = await request(server).get('/api/cards');
          // Make the assertion 
+         // too general
          expect(response.status).toBe(200);
+         expect(response.body.length).toBe(2);
+         expect(response.body.some(c => c.englishTitle === 'card1')).toBeTruthy();
+         expect(response.body.some(c => c.englishTitle === 'card2')).toBeTruthy();
       });
    });
 
