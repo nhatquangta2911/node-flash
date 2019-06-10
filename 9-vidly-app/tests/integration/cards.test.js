fbb15dc662fa2f2ce0818ebf834b353a29ec53d4
Nhat Quang Ta <nhatquangta2911@gmail.com>
2019-06-10 17:20:40 +0700

    Testing Auth middleware separately

--- 9-vidly-app/tests/integration/cards.test.js
+++ 9-vidly-app/tests/integration/cards.test.js
@@ -22,69 +22,69 @@ describe('/api/cards', () => {
    });
 
    //TODO: One suite for each route
-   describe('GET /', () => {
-      it('should return all cards', async () => {
-         await Card.collection.insertMany([{
-               englishTitle: 'card1',
-               vietnameseTitle: 'thetuvung1',
-               image: 'img1.png',
-               example: 'card1 in your area =))',
-               type: 'IDIOM',
-               context: 'for testing'
-            },
-            {
-               englishTitle: 'card2',
-               vietnameseTitle: 'thetuvung2',
-               image: 'img2.png',
-               example: 'card2 in your area =))',
-               type: 'IDIOM',
-               context: 'for testing'
-            }
-         ])
-         const response = await request(server).get('/api/cards');
-         // Make the assertion 
-         // too general
-         expect(response.status).toBe(200);
-         expect(response.body.length).toBe(2);
-         expect(response.body.some(c => c.englishTitle === 'card1')).toBeTruthy();
-         expect(response.body.some(c => c.englishTitle === 'card2')).toBeTruthy();
-      });
+   // describe('GET /', () => {
+   //    it('should return all cards', async () => {
+   //       await Card.collection.insertMany([{
+   //             englishTitle: 'card1',
+   //             vietnameseTitle: 'thetuvung1',
+   //             image: 'img1.png',
+   //             example: 'card1 in your area =))',
+   //             type: 'IDIOM',
+   //             context: 'for testing'
+   //          },
+   //          {
+   //             englishTitle: 'card2',
+   //             vietnameseTitle: 'thetuvung2',
+   //             image: 'img2.png',
+   //             example: 'card2 in your area =))',
+   //             type: 'IDIOM',
+   //             context: 'for testing'
+   //          }
+   //       ])
+   //       const response = await request(server).get('/api/cards');
+   //       // Make the assertion 
+   //       // too general
+   //       expect(response.status).toBe(200);
+   //       expect(response.body.length).toBe(2);
+   //       expect(response.body.some(c => c.englishTitle === 'card1')).toBeTruthy();
+   //       expect(response.body.some(c => c.englishTitle === 'card2')).toBeTruthy();
+   //    });
 
-   });
+   // });
 
-   describe('GET /:id', () => {
-      it('should return a card if valid id is passed', async () => {
-         const id1 = mongoose.Types.ObjectId();
-         const id2 = mongoose.Types.ObjectId();
-         await Card.collection.insertMany([
-            {
-               _id: id1,
-               englishTitle: 'card1',
-               vietnameseTitle: 'thetuvung1',
-               image: 'img1.png',
-               example: 'card1 in your area =))',
-               type: 'IDIOM',
-               context: 'for testing'
-            },
-            {
-               _id: id2,
-               englishTitle: 'card2',
-               vietnameseTitle: 'thetuvung2',
-               image: 'img2.png',
-               example: 'card2 in your area =))',
-               type: 'IDIOM',
-               context: 'for testing'
-            }
-         ]);
-         const response = await request(server).get(`/api/cards/card/${id1}`);
-         expect(response.status).toBe(200);
-         expect(response.body).toHaveProperty('englishTitle', 'card1');
-      });
-      it('should return 404 if invalid id is passed', async () => {
-         const response = await request(server).get('/api/cards/card/1');
-         expect(response.status).toBe(404);
-      });
-   });
+   // describe('GET /:id', () => {
+   //    it('should return a card if valid id is passed', async () => {
+   //       const id1 = mongoose.Types.ObjectId();
+   //       const id2 = mongoose.Types.ObjectId();
+   //       await Card.collection.insertMany([
+   //          {
+   //             _id: id1,
+   //             englishTitle: 'card1',
+   //             vietnameseTitle: 'thetuvung1',
+   //             image: 'img1.png',
+   //             example: 'card1 in your area =))',
+   //             type: 'IDIOM',
+   //             context: 'for testing'
+   //          },
+   //          {
+   //             _id: id2,
+   //             englishTitle: 'card2',
+   //             vietnameseTitle: 'thetuvung2',
+   //             image: 'img2.png',
+   //             example: 'card2 in your area =))',
+   //             type: 'IDIOM',
+   //             context: 'for testing'
+   //          }
+   //       ]);
+   //       const response = await request(server).get(`/api/cards/card/${id1}`);
+   //       expect(response.status).toBe(200);
+   //       expect(response.body).toHaveProperty('englishTitle', 'card1');
+   //    });
+   //    it('should return 404 if invalid id is passed', async () => {
+   //       const response = await request(server).get('/api/cards/card/1');
+   //       expect(response.status).toBe(404);
+   //    });
+   // });
 
    describe('POST /', () => {
       
