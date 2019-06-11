daa98fd274e02c5670117d9eb281eb6075059767
Nhat Quang Ta <nhatquangta2911@gmail.com>
2019-06-11 11:48:40 +0700

    Broadcasting to other clients

--- 16-websockets/index.js
+++ 16-websockets/index.js
@@ -21,4 +21,8 @@ io.on('connection', (socket) => {
    socket.on('chat', (data) => {
       io.sockets.emit('chat', data);
    });
+
+   socket.on('typing', (data) => {
+      socket.broadcast.emit('typing', data);
+   })
 });
--- 16-websockets/public/chat.js
+++ 16-websockets/public/chat.js
@@ -5,6 +5,7 @@ var message = document.getElementById('message');
 var handle = document.getElementById('handle');
 var btn = document.getElementById('send');
 var output = document.getElementById('output');
+var feedback = document.getElementById('feedback');
 
 // Emit events when click SEND
 btn.addEventListener('click', () => {
@@ -14,7 +15,16 @@ btn.addEventListener('click', () => {
    })
 });
 
+message.addEventListener('keypress', () => {
+   socket.emit('typing', handle.value);
+});
+
 // Listen for events
 socket.on('chat', (data) => {
    output.innerHTML += '<p><strong>' + data.handle + '</strong>' + data.message + '</p>';
+   feedback.innerHTML = '';
+});
+
+socket.on('typing', (data) => {
+   feedback.innerHTML = '<p><em><b>' + data + '</b> is typing a message... </em></p>';
 });
