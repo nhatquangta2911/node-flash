6ee5f395d12b3f394d36d8cf61d84fdc4583d671
Nhat Quang Ta <nhatquangta2911@gmail.com>
2019-06-21 10:19:36 +0700

    readline =))

--- 2-node-core/logger.js
+++ 2-node-core/logger.js
@@ -1,12 +1,22 @@
-const EventEmitter = require('events');
+// const EventEmitter = require('events');
 
-class Logger extends EventEmitter {
-   // Basic f() and additional methods
-   log(name) {
-      console.log(name);
-      // Raise an event 
-      this.emit('messageLogged', { id: 1, url: 'http://' });
-   } 
-}
+// class Logger extends EventEmitter {
+//    // Basic f() and additional methods
+//    log(name) {
+//       console.log(name);
+//       // Raise an event 
+//       this.emit('messageLogged', { id: 1, url: 'http://' });
+//    } 
+// }
 
-module.exports = Logger;
+// module.exports = Logger;
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+   input: process.stdin,
+   output: process.stdout
+ });
+
+
+const input = rl.question('Input plz: ', data => data);
